384603c068bb58fd50de23da2b6082ba79c13440
inesrf <inesrf.art@gmail.com>
2024-06-18 11:04:55 +0200

    pequeños cambios css

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import logo from "../assets/logo.svg";
+import userIcon from "../assets/user-icon.png";
 import { NavItem } from "./types";
 
 const navItems: NavItem[] = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
-  { name: "Services", href: "/services" },
+  { name: "Courses", href: "/courses" },
   { name: "Contact", href: "/contact" },
 ];
 
 const NavBar: React.FC = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [activeItem, setActiveItem] = useState<string>(navItems[0].name);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleItemClick = (itemName: string) => {
+    setActiveItem(itemName);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -27,10 +39,29 @@ const NavBar: React.FC = () => {
         <ul className="navbar-menu">
           {navItems.map((item) => (
             <li key={item.name} className="navbar-link">
-              <a href={item.href}>{item.name}</a>
+              <a
+                href={item.href}
+                className={activeItem === item.name ? "active" : ""}
+                onClick={() => handleItemClick(item.name)}
+              >
+                {item.name}
+              </a>
             </li>
           ))}
         </ul>
+
+        <div className="navbar-login-container">
+          <button className="navbar-login-icon" onClick={toggleDropdown}>
+            <img src={userIcon} alt="User Icon" className="user-icon" />
+          </button>
+          {dropdownVisible && (
+            <div className="navbar-login-dropdown">
+              <a href="/login">Log In</a>
+              <a href="/signup">Sign Up</a>
+            </div>
+          )}
+        </div>
+
         <button className="navbar-toggle">
           <svg
             fill="none"
