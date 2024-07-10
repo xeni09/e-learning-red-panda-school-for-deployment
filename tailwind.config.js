839fafc57680776cefd9386fd23e06c9942ff834
inesrf <inesrf.art@gmail.com>
2024-07-10 10:57:11 +0200

    configuracion tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/**/*.css", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
