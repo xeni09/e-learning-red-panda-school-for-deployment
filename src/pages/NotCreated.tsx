aa016f593d8f4589c3fd7258ac60ff9e306e74e9
inesrf <inesrf.art@gmail.com>
2024-07-15 15:34:24 +0200

    mas pequeños cambios de css

---
+++ src/pages/NotCreated.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NotCreated: React.FC = () => {
+  return (
+    <div className='container'>
+      <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-dark-900">
+        Pages not yet created or not necessary for the proyect
+      </h2>
+      <div className="mt-10 space-y-6 text-center">
+        <p className="text-sm text-gray-600">
+          This page is a placeholder for links that are not yet created.
+        </p>
+    
+      </div>
+    </div>
+  );
+};
+
+export default NotCreated;
