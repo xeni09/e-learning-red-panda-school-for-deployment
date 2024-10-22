3bcc4de3f7294f7fe6a46f25b76c65de17576d4e
inesrf <inesrf.art@gmail.com>
2024-10-22 16:47:39 +0200

    arreglos para localhost tb

--- ui/src/services/axiosConfig.js
+++ ui/src/services/axiosConfig.js
@@ -5,7 +5,7 @@ const baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 // Crear una instancia de Axios con configuración predefinida
 const axiosInstance = axios.create({
   baseURL,
-  withCredentials: true, // Asegura que las cookies se envíen con cada solicitud
+  withCredentials: true,
 });
 
 // Interceptor para manejar respuestas 401
@@ -14,7 +14,15 @@ axiosInstance.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 401) {
       console.error("Unauthorized, redirecting to login...");
-      window.location.href = "/login"; // Redirigir a la página de inicio de sesión para cualquier error 401
+      // Redirigir solo si estás en una página protegida
+      const protectedRoutes = ["/dashboard", "/my-account", "/admin"];
+      if (
+        protectedRoutes.some((route) =>
+          window.location.pathname.startsWith(route)
+        )
+      ) {
+        window.location.href = "/login";
+      }
     }
     return Promise.reject(error);
   }
